9d8663a3336c180fa85ab3eb1807f5018bb6fb29
MaryMuriuki <marymuriukinn@gmail.com>
2022-08-23 14:57:42 -0700

    commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,10 +7,10 @@ else:
     last = number % 10
     if last > 5:
         print("Last digit of {} is {} and is\
-                greater than 5".format(number, last))
+greater than 5".format(number, last))
     elif last == 0:
         print("Last digit of {} is {}\
-                and is 0".format(number, last))
+and is 0".format(number, last))
     elif last < 6 and last != 0:
         print("Last digit of {} is {} and is\
-                less than 6 and not 0".format(number, last))
+less than 6 and not 0".format(number, last))
