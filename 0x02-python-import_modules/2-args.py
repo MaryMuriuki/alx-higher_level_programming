14f6c91056636c8a17d4075f7bedcc7e5cb31b6d
MaryMuriuki <marymuriukinn@gmail.com>
2022-08-25 15:23:28 -0700

    commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    dig = len(sys.argv)
+    if dig == 1:
+        print("{} arguments.".format(dig - 1))
+    elif dig == 2:
+        print("{} arguments.".format(dig - 1))
+    else:
+        print("{} arguments:".format(dig - 1))
+
+    for i in range(1, dig):
+        print("{}: {}".format(i, sys.argv[i]))
