00bed39df4655cba61d287363fa097a8b81e4c3b
MaryMuriuki <marymuriukinn@gmail.com>
2022-08-25 15:07:01 -0700

    commit

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+    a: first int
+    b: second int
+
+    Returns: 
+        The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    return (a /b)
