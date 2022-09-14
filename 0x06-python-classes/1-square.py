2b22b879627ea613e6f3916030525ff0dbb44db4
MaryMuriuki <marymuriukinn@gmail.com>
2022-09-14 16:04:51 -0700

    commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 class Square:
     """Represents a square.
-        Private instance attribute: size.
-            Instantiation with size (no type/value verification).
-                """
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification.)
+    """
     def __init__(self, size):
+        """Initializess data."""
         self.__size = size
